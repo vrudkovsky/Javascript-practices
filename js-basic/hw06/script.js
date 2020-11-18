6da7cf5400eeb0d99773fcaf915e826e8deca767
Victor Rudkovsky <vpcomtek@gmail.com>
2020-11-18 10:24:43 +0200

    -- forEach

---
+++ js-basic/hw06/script.js
@@ -0,0 +1,18 @@
+// const arr = ['hello', 'world', 23, '23', null];
+const arr = ['hello', true, 'world', sayHi = () => {alert('Hello')}, 23, false, '23', null, 15, undefined, {name: 'John'}, [0, 1, 2, 3], 'wow'];
+
+function filterBy(array, type) {
+    let newArray = [];
+
+    array.forEach((item) => {
+        if (typeof item !== type) {
+            newArray.push(item)
+        }
+    })
+    console.log(newArray);
+}
+console.log(typeof arr);
+filterBy(arr, 'string');
+filterBy(arr, 'array');
+filterBy(arr, 'null');
+filterBy(arr, 'object');
