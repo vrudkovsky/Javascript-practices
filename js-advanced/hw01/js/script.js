af05b2289891b6a67b39389fbf7d49284d649c3e
Victor Rudkovsky <vpcomtek@gmail.com>
2020-11-27 13:58:34 +0200

    -- Hamburger ES5

---
+++ js-advanced/hw01/js/script.js
@@ -0,0 +1,99 @@
+/**
+ * Класс, объекты которого описывают параметры гамбургера.
+ *
+ * @constructor
+ * @param size        Размер
+ * @param stuffing    Начинка
+ * @throws {HamburgerException}  При неправильном использовании
+ */
+function Hamburger(size, stuffing) {
+
+}
+
+/* Размеры, виды начинок и добавок */
+Hamburger.SIZE_SMALL = {size: 'small', price: 50, calories: 20};
+Hamburger.SIZE_LARGE = {size: 'large', price: 100, calories: 40};
+Hamburger.STUFFING_CHEESE = {stuffing: 'cheese', price: 10, calories: 20};
+Hamburger.STUFFING_SALAD = {stuffing: 'salad', price: 20, calories: 5};
+Hamburger.STUFFING_POTATO = {stuffing: 'potato', price: 15, calories: 10};
+Hamburger.TOPPING_MAYO = {stuffing: 'mayo', price: 20, calories: 5};
+Hamburger.TOPPING_SPICE = {stuffing: 'spice', price: 15, calories: 0};
+
+/**
+ * Добавить добавку к гамбургеру. Можно добавить несколько
+ * добавок, при условии, что они разные.
+ *
+ * @param topping     Тип добавки
+ * @throws {HamburgerException}  При неправильном использовании
+ */
+Hamburger.prototype.addTopping = function(topping) {
+
+}
+
+/**
+ * Убрать добавку, при условии, что она ранее была
+ * добавлена.
+ *
+ * @param topping   Тип добавки
+ * @throws {HamburgerException}  При неправильном использовании
+ */
+Hamburger.prototype.removeTopping = function(topping) {
+
+}
+
+/**
+ * Получить список добавок.
+ *
+ * @return {Array} Массив добавленных добавок, содержит константы
+ *                 Hamburger.TOPPING_*
+ */
+Hamburger.prototype.getToppings = function() {
+
+}
+
+/**
+ * Узнать размер гамбургера
+ */
+Hamburger.prototype.getSize = function () {
+
+}
+
+/**
+ * Узнать начинку гамбургера
+ */
+Hamburger.prototype.getStuffing = function () {
+
+}
+
+/**
+ * Узнать цену гамбургера
+ * @return {Number} Цена в тугриках
+ */
+Hamburger.prototype.calculatePrice = function () {
+
+}
+
+/**
+ * Узнать калорийность
+ * @return {Number} Калорийность в калориях
+ */
+Hamburger.prototype.calculateCalories = function () {
+
+}
+
+/**
+ * Представляет информацию об ошибке в ходе работы с гамбургером.
+ * Подробности хранятся в свойстве message.
+ * @constructor
+ */
+function HamburgerException () {
+
+}
+
+
+const hamburger = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE);
+const hamburgerException = new HamburgerException();
+
+console.log(hamburger);
+console.log(hamburgerException);
+
